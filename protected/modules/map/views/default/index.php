8424a23eaa6e16272b4634dd7d349b877544a666
Eric Goldhagen <eric@openflows.com>
2013-02-17 15:42:58 -0500

    adding in files that were in the work that Mariann did but were not in the repo, this is related to display of the map on the site

--- protected/modules/map/views/default/index.php
+++ protected/modules/map/views/default/index.php
@@ -1,17 +1,48 @@
-<?php
-/* @var $this DefaultController */
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width">
 
-$this->breadcrumbs=array(
-	$this->module->id,
-);
+        <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+
+        <link rel="stylesheet" href="styles/e62a0ad3.leaflet.css">
+        <link rel="stylesheet" href="styles/bc43c441.main.css">
+        <script src="scripts/vendor/cf69c6f2.modernizr.min.js"></script>
+    </head>
+    <body>
+    <div id="app"></div>
+
+    <!--[if lt IE 7]>
+        <p class="chromeframe">You are using an outdated browser. <a href="http://browsehappy.com/">Upgrade your browser today</a> or <a href="http://www.google.com/chromeframe/?redirect=true">install Google Chrome Frame</a> to better experience this site.</p>
+    <![endif]-->
+
+    <!-- Add your site or application content here -->
+
+    <!--<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.0/jquery.min.js"></script> -->
+    <script>window.jQuery || document.write('<script src="scripts/vendor/8bc61845.jquery.min.js"><\/script>')</script>
+
+<?php 
+
+if(Yii::app()->user->isGuest) {
+// something else needs to happen
+echo 'You have to log in to interact with the map';
 ?>
-<h1><?php echo $this->uniqueId . '/' . $this->action->id; ?></h1>
 
-<p>
-This is the view content for action "<?php echo $this->action->id; ?>".
-The action belongs to the controller "<?php echo get_class($this); ?>"
-in the "<?php echo $this->module->id; ?>" module.
-</p>
-<p>
-You may customize this page by editing <tt><?php echo __FILE__; ?></tt>
-</p>
+
+<?php } else { ?>
+    <script src="scripts/34ab0b8a.plugins.js"></script>
+
+    <script>
+      L.Icon.Default.imagePath = "/images"
+    </script>
+<?php }
+?>	
+</body>
+</html>
