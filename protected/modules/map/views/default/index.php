8b359a135ba3f3a58320d2c3ac8bcbe9e1333466
Juan C. Müller <jcmuller@gmail.com>
2013-02-22 01:27:48 -0500

    Don't protect on the view. Pass in options to map

--- protected/modules/map/views/default/index.php
+++ protected/modules/map/views/default/index.php
@@ -1,25 +1,36 @@
 <?php
-  if (Yii::app()->user->isGuest) {
-    // something else needs to happen
-    echo 'You have to log in to interact with the map';
-  } else {
-    //$BASE_URL="http://localhost:8000/";
-    $BASE_URL="http://digidem.github.com/HaitiHackMap/";
+    $BASE_URL="http://localhost:8000/";
+    //$BASE_URL="http://digidem.github.com/HaitiHackMap/";
     $this->layout = 'simple';
 ?>
-    <div id="app"></div>
-    <link rel="stylesheet" href="<?php print $BASE_URL ?>styles/main.css">
-    <link rel="stylesheet" href="<?php print $BASE_URL ?>styles/leaflet.css">
-    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.0/jquery.min.js"></script>
-    <script>window.jQuery || document.write('<script src="<?php print $BASE_URL ?>scripts/vendor/jquery.min.js"><\/script>')</script>
-    <script src="<?php print $BASE_URL ?>scripts/plugins.js"></script>
 
-    <script>
-      window.categories_url = "index.php?r=category/index";
-      window.results_url = "results.json";
-      L.Icon.Default.imagePath = "<?php print $BASE_URL ?>images"
-      new window.App.Router();
-    </script>
-<?php
-  }
-?>
+<div id="app"></div>
+<link rel="stylesheet" href="<?php print $BASE_URL ?>styles/main.css">
+<link rel="stylesheet" href="<?php print $BASE_URL ?>styles/leaflet.css">
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.0/jquery.min.js"></script>
+<script>window.jQuery || document.write('<script src="<?php print $BASE_URL ?>scripts/vendor/jquery.min.js"><\/script>')</script>
+<script src="<?php print $BASE_URL ?>scripts/plugins.js"></script>
+
+<script>
+    options = {
+        leafletImagesPath: "<?php print $BASE_URL ?>images",
+
+        categoriesUrl: "index.php?r=category",
+        resultsUrl: "index.php?r=branchsite",
+
+        extractLocation: function(model) {
+          return [model.get('latitude'), model.get('longitude')];
+        },
+
+        titleRenderer: function(model) {
+          return '<h6>' +
+            model.get('organisation_name') + ' / ' +
+            model.get('quartier_name') + ' / ' +
+            model.get('branch_name') +
+            '</h6>'
+        },
+        detailsRenderer: function(model) { return '<note>' + model.get('street_address') + '</note>'},
+    };
+
+    new HaitiHackMap(options);
+</script>
