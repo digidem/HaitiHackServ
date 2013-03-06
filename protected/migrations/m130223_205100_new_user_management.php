432e9a5af621450e3497018d1f080e087322d705
Mariann Urban <urbanm@unhcr.org>
2013-03-06 18:40:10 +0100

    Bug fix #2 for the migration scripts. apologies.

--- protected/migrations/m130223_205100_new_user_management.php
+++ protected/migrations/m130223_205100_new_user_management.php
@@ -19,7 +19,7 @@ class m130223_205100_new_user_management extends CDbMigration
 
         ));
         
-        $this->createIndex('users_pk', 'users', 'id', true);	
+	
         $this->createIndex('users_email', 'users', 'email', true);	
         $this->createIndex('users_username', 'users', 'username', true);	
         $this->createIndex('users_status', 'users', 'status', false);	
@@ -33,13 +33,38 @@ class m130223_205100_new_user_management extends CDbMigration
 	public function down()
 	{
 
-        $this->dropIndex('users_pk');	
+
         $this->dropIndex('users_email');	
         $this->dropIndex('users_username');	
         $this->dropIndex('users_status');	
         $this->dropIndex('users_superuser');	
         $this->dropTable('users');	
-	
+		
+		$this->createTable('users', array(
+            'id' => 'pk',
+            'username' => 'VARCHAR (20) NOT NULL',
+            'fullname' => 'VARCHAR (150) NOT NULL',
+            'password' => 'VARCHAR (150) NOT NULL',
+            'email' => 'VARCHAR (100) NOT NULL',
+            'level' => 'VARCHAR (45) NOT NULL',
+        ));		
+		
+		$this->insert('users', array(
+            'id' => '1',
+            'username' => 'admin',
+            'fullname' => 'XXX',
+            'password' => '098f6bcd4621d373cade4e832627b4f6',
+            'email' => 'xxx@example.com',
+            'level' => 'Admin',
+        ));		
+		$this->insert('users', array(
+            'id' => '2',
+            'username' => 'simple',
+            'fullname' => 'Simple User',
+            'password' => '098f6bcd4621d373cade4e832627b4f6',
+            'email' => 'xxx@example.com',
+            'level' => 'Basic',
+        ));			
 	}
 
 	/*
--- protected/migrations/m130305_103857_new_user_management_profiles.php
+++ protected/migrations/m130305_103857_new_user_management_profiles.php
@@ -9,11 +9,8 @@ class m130305_103857_new_user_management_profiles extends CDbMigration
             'user_id' => 'pk',
 			'firstname' => 'VARCHAR (50) NOT NULL DEFAULT \'\'',
 			'lastname' => 'VARCHAR (50) NOT NULL DEFAULT \'\'',
-
         ));
-        
-
-	
+      	
 	}
 
 	public function down()
